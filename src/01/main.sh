3cc8556987bfbcbc5307cabfe10ee64a7a5869d7
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-01-05 13:52:40 +0300

    develop FIX PART 1 PARSE ALL ARG TO EXSITING NUM

--- src/01/main.sh
+++ src/01/main.sh
@@ -2,12 +2,10 @@
 
 if [ "$#" -eq 0 ] || [ "$#" -gt 1 ] ;
 then 
-    echo "NOT SET PARAM"
+    echo "PLESASE SET ONE PARAM"
 else
-    if [[ $1 =~ ^[a-z] ]] ;
-    then
-        echo $1
-    else
-        echo "ENTER TEXT PARAM"
-    fi
+    case $1 in
+        ''|*[!a-zA-Z]*) echo ENTER TEXT PARAM ;;
+        *) echo $1 ;;
+    esac
 fi
