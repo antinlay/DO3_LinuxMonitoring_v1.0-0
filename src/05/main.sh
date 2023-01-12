d3987207f5c887cf0ec01a02b3c5343278c4588d
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-01-12 18:36:42 +0300

    develop FIX ALL DETAILS FOR ALL PARTS

--- src/05/main.sh
+++ src/05/main.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-declare dir_pr=$(pwd) dir_0="/05" get_dir="${@}" start_time=$(date +%s.%N);
+declare dir_pr=$(pwd) dir_0="/05" get_dir=$(dirname "${1}") start_time=$(date +%s.%N);
 
 if [[ $dir_pr == *"$dir_0" ]] ;
 then
+  if [[ $# -gt 1 ]];
+  then
+      echo "Error: Please run script with single parameter."
+      exit 1
+  fi
+  if [[ "${1}" != */ ]];
+  then
+      echo "Error: Please atention parameter must end with '/'."
+      exit 1
+  fi
   if test -d "${get_dir}";
   then 
     # Get DIR
