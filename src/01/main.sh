8aff2348ba1bf9b5fab43f03353922de69d3aade
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-01-12 17:22:55 +0300

    develop FIX PART 1

--- src/01/main.sh
+++ src/01/main.sh
@@ -4,7 +4,10 @@ if [ "$#" -eq 1 ] ;
 then 
     if [[ "${1}" =~ ^[-+]?[0-9]+([.,][0-9]+)?$ ]]; then
         echo "Error: enter incorrect" >&2
+        exit 1
     else
         echo $1
     fi
+else
+    exit 1
 fi
