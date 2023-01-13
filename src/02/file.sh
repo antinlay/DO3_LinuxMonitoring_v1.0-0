e5dd8cad788a1ca42feb016f820e7ba50aaa9a4b
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-01-14 00:18:33 +0300

    develop FIX PERMISSION

--- src/02/file.sh
+++ src/02/file.sh
@@ -2,13 +2,13 @@
 
 declare temp=".temp";
 
-./echo.sh | tee "${temp}";
+bash echo.sh | tee "${temp}";
+trap "rm -f ${temp}" EXIT;
 echo "Create file with sysinfo? (y - yes, other - no)";
 read -s item;
 case "$item" in
     y|Y) mv ${temp} $(date "+%d_%m_%Y_%H_%M_%S.status")
         echo "Report was created!" ;;
-    *) rm "${temp}";
-        echo "File didn't created"
+    *) echo "File didn't created"
         exit 0 ;;
 esac
